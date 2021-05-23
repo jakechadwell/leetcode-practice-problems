cdff8a80a28f279bd3545cdb116279f749b0aad2
Jake Chadwell <jake.chadwell01@gmail.com>
2021-05-22 23:23:24 -0500

    completed add two numbers problem

--- javascript-practice-problems.js
+++ javascript-practice-problems.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 "use strict";
 // Write a function named countPosi that accepts an array of numbers and returns the number of positive numbers in the array.
 // countPosi([-2, 7, 4, -9, 1]) //3
@@ -11,7 +10,7 @@ function countPosi(x) {
             count++
         }
     } return count
-};
+}
 
 // Write a function named convertAddressToObject that accepts a string that contains a street number (as a string) and street name separated by a space characters, and returns an object with properties streetNumber and streetName.
 // convertAddressToObject('8626 Sunny Oaks') // {streetNumber: '8646', streetName: 'Sunny Oaks'}
@@ -23,7 +22,7 @@ function convertAddressToObject(string){
     address.streetNumber = array[0];
     address.streetName = array[1] +" " + array[2];
     console.log(address);
-  };
+  }
 
 /**
  * 28.Implement strStr()
@@ -41,7 +40,6 @@ var strStr = function(haystack, needle) {
  * 
  * 
  */
-=======
 /**
  * @param {string} s
  * @return {number}
@@ -56,4 +54,42 @@ var lengthOfLastWord = function(s) {
     }
 };
 console.log(lengthOfLastWord("a "));
->>>>>>> e1e6d9df5343bcd4ef96d951de8fd6ecd9c55d89
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} l1
+ * @param {ListNode} l2
+ * @return {ListNode}
+ */
+var addTwoNumbers = function(l1, l2) {
+    var List = new ListNode(0);
+    var head = List;
+    var sum = 0;
+    var carry = 0;
+
+    while(l1!==null||l2!==null||sum>0){
+        if(l1!==null){
+            sum = sum + l1.val;
+            l1 = l1.next;
+        }
+        if(l2!==null){
+            sum = sum + l2.val;
+            l2 = l2.next;
+        }
+        if(sum>=10){
+            carry = 1;
+            sum = sum - 10;
+        }
+        head.next = new ListNode(sum);
+        head = head.next;
+        sum = carry;
+        carry = 0;
+    }
+    return List.next;
+};
