7b97b2cd0382f1f5e13f06d7cb256b2e908bce59
Jake Chadwell <jake.chadwell01@gmail.com>
2021-06-01 22:19:43 -0500

    max profit problem

--- javascript-practice-problems.js
+++ javascript-practice-problems.js
@@ -181,3 +181,19 @@ var removeElement = function(nums, val) {
     }
     return count;
 };
+
+/**
+ * @param {number[]} prices
+ * @return {number}
+ */
+var maxProfit = function(prices) {
+    let maxprofit = 0;
+    for (let i = 0; i < prices.length - 1; i++) {
+        for (let j = i + 1; j < prices.length; j++) {
+            let profit = prices[j] - prices[i];
+            if (profit > maxprofit)
+                maxprofit = profit;
+        }
+    }
+    return maxprofit;
+};
