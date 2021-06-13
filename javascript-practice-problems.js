45a98c3eaeffec088cc7a5c555737af955d28ea5
Jake Chadwell <jake.chadwell01@gmail.com>
2021-06-12 22:52:13 -0500

    two sum complete

--- javascript-practice-problems.js
+++ javascript-practice-problems.js
@@ -238,3 +238,25 @@ var isMonotonic = function(nums) {
     return true;
 
 };
+
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(nums, target) {
+
+    var result = [];
+    for(var i=0;i<nums.length;i++){
+        for(var j=1;j<nums.length;j++ ){
+            if(nums[i]+ nums[j] === target){
+                result.push(i);
+                result.push(j)
+            }
+        }
+
+
+        return result;
+    }
+
+};
