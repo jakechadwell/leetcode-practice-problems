664863b3a671521903075885686d849d6d89ba49
Jake Chadwell <jake.chadwell01@gmail.com>
2021-07-04 23:13:28 -0500

    longest common prefix

--- javascript-practice-problems.js
+++ javascript-practice-problems.js
@@ -337,3 +337,15 @@ let romanToInt = function (str) {
     }
     return res;
 };
+
+/**
+ * @param {string[]} strs
+ * @return {string}
+ */
+var longestCommonPrefix = function(strs) {
+    strs.sort();
+    for (let i = 0; i < strs[0].length; i++) {
+        if (strs[0][i] !== strs[strs.length - 1][i]) return strs[0].substr(0, i);
+    }
+    return strs[0];
+};
