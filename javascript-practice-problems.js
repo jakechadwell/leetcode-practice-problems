e684682c2b64d038fd5e80fe50043bfeed3b085f
Jake Chadwell <jake.chadwell01@gmail.com>
2021-07-05 23:48:51 -0500

    merge sorted lists

--- javascript-practice-problems.js
+++ javascript-practice-problems.js
@@ -349,3 +349,42 @@ var longestCommonPrefix = function(strs) {
     }
     return strs[0];
 };
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} l1
+ * @param {ListNode} l2
+ * @return {ListNode}
+ */
+var mergeTwoLists = function(l1, l2) {
+    if (!l1)
+        return l2;
+
+    if (!l2)
+        return l1;
+
+    let list = new ListNode();
+    let pointer = list;
+
+    while (l1 && l2) {
+        if (l1.val <= l2.val) {
+            list.next = l1;
+            l1 = l1.next
+        } else {
+            list.next = l2;
+            l2 = l2.next;
+        }
+
+        list = list.next;
+    }
+
+    list.next = l1 || l2;
+
+    return pointer.next;
+};
